0f3e5d20388b5caf26cd3f6b395d12214a689b1a
anekos <anekos@snca.net>
2021-10-12 21:53:03 +0900

    DELETE `/alias/{name}`

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -59,10 +59,16 @@ async fn on_alias(data: web::Data<Mutex<AppData>>, name: web::Path<String>) -> A
     Ok(HttpResponse::Ok().json(alias))
 }
 
+async fn on_alias_delete(data: web::Data<Mutex<AppData>>, name: web::Path<String>) -> AppResult<HttpResponse> {
+    let data = data.lock().expect("lock delete alias");
+    data.db.delete_alias(&name)?;
+    Ok(HttpResponse::Ok().json(true))
+}
+
 async fn on_alias_update(data: web::Data<Mutex<AppData>>, name: web::Path<String>, alias: web::Json<Alias>) -> AppResult<HttpResponse> {
     let data = data.lock().expect("lock set alias");
     data.db.upsert_alias(&name, &alias.expression, alias.recursive)?;
-    Ok(HttpResponse::Ok().json(&*alias))
+    Ok(HttpResponse::Ok().json(true))
 }
 
 async fn on_aliases(data: web::Data<Mutex<AppData>>) -> AppResult<HttpResponse> {
@@ -150,7 +156,7 @@ pub async fn start(db: Database, aliases: GlobalAliasTable, port: u16, root: Str
     HttpServer::new(move || {
         let cors = Cors::default()
              .allow_any_origin()
-             .allowed_methods(vec!["GET", "POST"])
+             .allowed_methods(vec!["GET", "POST", "DELETE"])
              .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
              .allowed_header(http::header::CONTENT_TYPE)
              .max_age(3600);
@@ -159,6 +165,7 @@ pub async fn start(db: Database, aliases: GlobalAliasTable, port: u16, root: Str
             .app_data(data.clone())
             .service(web::resource("/alias/{name}")
                 .route(web::get().to(on_alias))
+                .route(web::delete().to(on_alias_delete))
                 .route(web::post().to(on_alias_update)))
             .service(web::resource("/aliases").route(web::get().to(on_aliases)))
             .service(web::resource("/download").route(web::post().to(on_download)))
