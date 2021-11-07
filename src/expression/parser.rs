f52123f75c0f3c08f11a9bc738a33dcd2ce708e9
anekos <anekos@snca.net>
2021-11-07 10:21:20 +0900

    Make `tests` mod

--- src/expression/parser.rs
+++ src/expression/parser.rs
@@ -23,58 +23,15 @@ fn delimiter(input: &str) -> IResult<&str, E> {
     Ok((rest, E::Delimiter(x.iter().collect())))
 }
 
-#[test]
-fn parse_delimiter() {
-    use E::{Delimiter as D};
-
-    assert_eq!(
-        delimiter(" \t"),
-        Ok(("", D(" \t".to_owned())))
-    );
-
-    assert_eq!(
-        delimiter(" \t\r\n123"),
-        Ok(("123", D(" \t\r\n".to_owned())))
-    );
-}
-
 fn noir_tag(input: &str) -> IResult<&str, E> {
     let (rest, _) = cchar('#')(input)?;
     let (rest, y) = many1(none_of("\"() \t\r\n"))(rest)?;
     Ok((rest, E::NoirTag(y.iter().collect())))
 }
 
-#[test]
-fn parse_noir_tag() {
-    use E::{NoirTag as T};
-
-    assert_eq!(
-        noir_tag(r#"#foo"#),
-        Ok(("", T("foo".to_owned())))
-    );
-}
-
 fn term(input: &str) -> IResult<&str, E> {
     let (rest, x) = many1(none_of(DELIMITERS))(input)?;
     Ok((rest, E::Term(x.iter().collect())))
-}
-
-#[test]
-fn parse_term() {
-    use E::{Term as T};
-
-    assert_eq!(
-        term(r#"cat-dog"#),
-        Ok(("", T("cat-dog".to_owned())))
-    );
-    assert_eq!(
-        term(r#"cat"#),
-        Ok(("", T("cat".to_owned())))
-    );
-    assert_eq!(
-        term(r#"cat and dog"#),
-        Ok((" and dog", T("cat".to_owned())))
-    );
 }
 
 fn string_literal(input: &str) -> IResult<&str, E> {
@@ -90,53 +47,101 @@ fn string_literal(input: &str) -> IResult<&str, E> {
     Ok((rest, E::StringLiteral(result.iter().collect())))
 }
 
-#[test]
-fn parse_string_literal() {
-    use E::{StringLiteral as S};
-
-    assert_eq!(
-        string_literal(r#"'cat'"#),
-        Ok(("", S("cat".to_owned())))
-    );
-    assert_eq!(
-        string_literal(r#"'A'"#),
-        Ok(("", S("A".to_owned())))
-    );
-
-    assert_eq!(
-        string_literal(r#"'A'''"#),
-        Ok(("", S("A'".to_owned())))
-    );
-    assert_eq!(
-        string_literal(r#"'A''B'"#),
-        Ok(("", S("A'B".to_owned())))
-    );
-    assert_eq!(
-        string_literal(r#"'A''B''C'"#),
-        Ok(("", S("A'B'C".to_owned())))
-    );
-    assert_eq!(
-        string_literal(r#"'A''B''''C'"#),
-        Ok(("", S("A'B''C".to_owned())))
-    );
-    assert_eq!(
-        string_literal(r#"'A''B''''C'RR"#),
-        Ok(("RR", S("A'B''C".to_owned())))
-    );
-}
-
 pub fn parse(input: &str) -> IResult<&str, Vec<E>> {
     let p = alt((noir_tag, term, string_literal, delimiter, any));
     many0(p)(input)
 }
 
-#[test]
-fn test_parse() {
-    assert_eq!(
-        parse(r#"()"#),
-        Ok(
-            ("",
-             vec![
-                 E::Delimiter("()".to_owned())
-             ])));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_delimiter() {
+        use E::{Delimiter as D};
+
+        assert_eq!(
+            delimiter(" \t"),
+            Ok(("", D(" \t".to_owned())))
+        );
+
+        assert_eq!(
+            delimiter(" \t\r\n123"),
+            Ok(("123", D(" \t\r\n".to_owned())))
+        );
+    }
+
+    #[test]
+    fn test_noir_tag() {
+        use E::{NoirTag as T};
+
+        assert_eq!(
+            noir_tag(r#"#foo"#),
+            Ok(("", T("foo".to_owned())))
+        );
+    }
+
+    #[test]
+    fn test_string_literal() {
+        use E::{StringLiteral as S};
+
+        assert_eq!(
+            string_literal(r#"'cat'"#),
+            Ok(("", S("cat".to_owned())))
+        );
+        assert_eq!(
+            string_literal(r#"'A'"#),
+            Ok(("", S("A".to_owned())))
+        );
+
+        assert_eq!(
+            string_literal(r#"'A'''"#),
+            Ok(("", S("A'".to_owned())))
+        );
+        assert_eq!(
+            string_literal(r#"'A''B'"#),
+            Ok(("", S("A'B".to_owned())))
+        );
+        assert_eq!(
+            string_literal(r#"'A''B''C'"#),
+            Ok(("", S("A'B'C".to_owned())))
+        );
+        assert_eq!(
+            string_literal(r#"'A''B''''C'"#),
+            Ok(("", S("A'B''C".to_owned())))
+        );
+        assert_eq!(
+            string_literal(r#"'A''B''''C'RR"#),
+            Ok(("RR", S("A'B''C".to_owned())))
+        );
+    }
+
+    #[test]
+    fn test_term() {
+        use E::{Term as T};
+
+        assert_eq!(
+            term(r#"cat-dog"#),
+            Ok(("", T("cat-dog".to_owned())))
+        );
+        assert_eq!(
+            term(r#"cat"#),
+            Ok(("", T("cat".to_owned())))
+        );
+        assert_eq!(
+            term(r#"cat and dog"#),
+            Ok((" and dog", T("cat".to_owned())))
+        );
+    }
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            parse(r#"()"#),
+            Ok(
+                ("",
+                 vec![
+                     E::Delimiter("()".to_owned())
+                 ])));
+    }
 }
