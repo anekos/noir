c89c5ba2b08635c7cd515db77e8cbfb1bcfd8295
anekos <anekos@snca.net>
2021-11-11 20:27:00 +0900

    Remove and add the index of `tags`

--- src/sql/create_tags_index.sql
+++ src/sql/create_tags_index.sql
@@ -1,2 +1,1 @@
-CREATE INDEX IF NOT EXISTS tags_index_path ON tags(tag, path);
-CREATE INDEX IF NOT EXISTS tags_index_path_source ON tags(tag, path, source);
+CREATE INDEX IF NOT EXISTS tags_index_path ON tags(path);
